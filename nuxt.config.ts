9ca2ee9f17f91f57469ccfa704639634f88b8fe4
Alexander Opalic <opalic@gmx.net>
2025-06-07 10:27:15 +0200

    added unco css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  css: [
-    // Add your global CSS files here
-    '~/assets/css/main.css',
-    // You can also add CSS frameworks or libraries
-    // '~/assets/css/tailwind.css',
-    // You can reference node_modules directly
-    // 'normalize.css'
-  ],
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
     '@nuxt/image',
     'nuxt-codemirror',
-    'reka-ui/nuxt'
-  ]
+    'reka-ui/nuxt',
+    '@unocss/nuxt'
+  ],
 })
