cbeaf155eb0b3c57ecdf41e001782f072daa58fe
Alexander Opalic <opalic@gmx.net>
2025-06-07 12:02:03 +0200

    little refactor

--- app.vue
+++ app.vue
@@ -85,16 +85,10 @@ useHead({
       :style="{ width: `${leftPaneWidth}%` }"
     />
     
-    <!-- Resizable splitter -->
-    <div 
-      class="w-1 bg-[#1d1f23] hover:bg-[#2a2d3a] cursor-col-resize flex-shrink-0 relative group transition-colors duration-200"
-      :class="{ 'bg-[#5e6ad2]': isDragging }"
-      @mousedown="startDrag"
-    >
-      <div class="absolute inset-0 flex items-center justify-center">
-        <div class="w-0.5 h-8 bg-[#6c7383] group-hover:bg-[#9ca3af] transition-colors duration-200 opacity-60"/>
-      </div>
-    </div>
+    <ResizableSplitter 
+      :is-dragging="isDragging"
+      @start-drag="startDrag"
+    />
     
     <MarkdownPreview 
       :rendered-html="renderedMarkdown"
